2e8035f15176ce7a8ec20e0a5e1390a759653cf2
Mityaiy <zdarovbratis@gmail.com>
2025-06-14 21:45:53 +0400

    Add files via upload

---
+++ miniproekty-php-dlya-novichkov-1/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ru">
+	<head>
+		<meta charset="utf-8">  
+		<title>Гостевая книга</title>
+		<link rel="stylesheet" href="css/bootstrap/css/bootstrap.css">
+		<link rel="stylesheet" href="css/styles.css">
+	</head>
+	<body>
+		<div id="wrapper">
+			<?php 
+			
+				foreach ($arr as $text) {
+					echo $text;
+				}
+			
+			?>
+			<div class="info alert alert-info">
+				Запись успешно сохранена!
+			</div>
+			<div id="form">
+				<form action="#form" method="POST">
+					<p><input class="form-control" placeholder="Ваше имя"></p>
+					<p><textarea class="form-control" placeholder="Ваш отзыв"></textarea></p>
+					<p><input type="submit" class="btn btn-info btn-block" value="Сохранить"></p>
+				</form>
+			</div>
+		</div>
+	</body>
+</html>
+
